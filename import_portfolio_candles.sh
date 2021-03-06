18374537eea57a2989bc9e6df5a8202c1ffe5199
Pablo Cerda <pablo@citla.com>
2021-03-06 22:18:19 +0100

    Bash script to import astro research coins data

---
+++ import_portfolio_candles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Import astro research portfolio coins data.
+jesse import-candles 'Binance' 'ADA-USDT' '2017-01-01' --skip-confirmation
+jesse import-candles 'Binance' 'BAT-USDT' '2017-01-01' --skip-confirmation
+jesse import-candles 'Binance' 'BNB-USDT' '2017-01-01' --skip-confirmation
+jesse import-candles 'Binance' 'BTC-USDT' '2017-01-01' --skip-confirmation
+jesse import-candles 'Binance' 'DASH-USDT' '2017-01-01' --skip-confirmation
+jesse import-candles 'Binance' 'EOS-USDT' '2017-01-01' --skip-confirmation
+jesse import-candles 'Binance' 'LINK-USDT' '2017-01-01' --skip-confirmation
+jesse import-candles 'Binance' 'LTC-USDT' '2017-01-01' --skip-confirmation
+jesse import-candles 'Binance' 'ZEC-USDT' '2017-01-01' --skip-confirmation
+jesse import-candles 'Binance' 'ZRX-USDT' '2017-01-01' --skip-confirmation
