e9cddd1b091781e2d3e8fab438df36ea0c9c0d79
Pablo Cerda <pablo@citla.com>
2021-03-08 11:19:22 +0100

    Only BTC-USDT active until capital split Jesse fix

--- routes.py
+++ routes.py
@@ -7,28 +7,28 @@ from jesse.utils import anchor_timeframe
 
 # trading routes
 routes = [
-    ('Binance', 'ADA-USDT', '15m', 'AstroStrategyMA'),
-    ('Binance', 'BAT-USDT', '15m', 'AstroStrategyMA'),
-    ('Binance', 'BNB-USDT', '15m', 'AstroStrategyMA'),
+    # ('Binance', 'ADA-USDT', '15m', 'AstroStrategyMA'),
+    # ('Binance', 'BAT-USDT', '15m', 'AstroStrategyMA'),
+    # ('Binance', 'BNB-USDT', '15m', 'AstroStrategyMA'),
     ('Binance', 'BTC-USDT', '15m', 'AstroStrategyMA'),
     # ('Binance', 'DASH-USDT', '15m', 'AstroStrategyMA'),
     # ('Binance', 'EOS-USDT', '15m', 'AstroStrategyMA'),
     # ('Binance', 'LINK-USDT', '15m', 'AstroStrategyMA'),
     # ('Binance', 'LTC-USDT', '15m', 'AstroStrategyMA'),
-    ('Binance', 'ZEC-USDT', '15m', 'AstroStrategyMA'),
+    # ('Binance', 'ZEC-USDT', '15m', 'AstroStrategyMA'),
     # ('Binance', 'ZRX-USDT', '15m', 'AstroStrategyMA'),
 ]
 
 # in case your strategy requires extra candles, timeframes, ...
 extra_candles = [
-    ('Binance', 'ADA-USDT', '1D'),
-    ('Binance', 'BAT-USDT', '1D'),
-    ('Binance', 'BNB-USDT', '1D'),
+    # ('Binance', 'ADA-USDT', '1D'),
+    # ('Binance', 'BAT-USDT', '1D'),
+    # ('Binance', 'BNB-USDT', '1D'),
     ('Binance', 'BTC-USDT', '1D'),
     # ('Binance', 'DASH-USDT', '1D'),
     # ('Binance', 'EOS-USDT', '1D'),
     # ('Binance', 'LINK-USDT', '1D'),
     # ('Binance', 'LTC-USDT', '1D'),
-    ('Binance', 'ZEC-USDT', '1D'),
+    # ('Binance', 'ZEC-USDT', '1D'),
     # ('Binance', 'ZRX-USDT', '1D'),
 ]
